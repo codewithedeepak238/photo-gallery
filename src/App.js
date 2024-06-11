7645a0bdaf7f65ad8b083b91943d1d62aef8be3a
Deepak Saraswat <deepaksaraswat238@gmail.com>
2024-06-11 23:21:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import { Header } from "./components/Header";
+import { TaskCard } from "./components/TaskCard";
 import "./App.css";
-import { CartCard } from "./components/CartCard";
-import {useCart} from "./contexts/CartContext"
+import { useState } from "react";
 
 export default function App() {
-  const {cartItem, total, clearCart} = useCart();
+  const [tasks, setTasks] = useState([]);
+  function handleAdd(e){
+    e.preventDefault();
+    const task = {
+      id: Math.random()*10*Math.random(),
+      taskName : e.target.input.value,
+      complete : false
+    }
+    e.target.input.value = ""
+    setTasks([...tasks, task])
+  }
   return (
-    <div className="App">
-      <Header/>
-      <div className="main">
-        <h1>YOUR BAG</h1>
+    <div className="App flex item-center justify-center">
+      <div className="rounded-[10px] bg-white shadow-xl p-[2%] text-center w-[35vw] mt-[10%]">
+        <h1 className="text-[24px] mb-[7%]">Grocery Bud</h1>
         <div>
+          <form onSubmit={handleAdd} className="flex items-center justify-center">
+            <input id="input" className="bg-[#f8fafc] focus:outline-none px-[5px] py-[2px] rounded-tl-[5px] flex-1 rounded-bl-[5px] border-[1px] border-[#e2e8f0]" type="text" required autoComplete="off"/>
+            <button className="text-white bg-blue-400 rounded-br-[5px] rounded-tr-[5px] px-[5px] py-[3px]">Add Item</button>
+          </form>
+        </div>
+        <div className="py-[3%] mt-[2%]">
           {
-            cartItem.map((item)=>(
-              <CartCard key={item.id} item={item}/>
+            tasks && tasks.map((task)=>(
+              <TaskCard key={task.id} task={task} tasks={tasks} setTasks={setTasks}/>
             ))
           }
-        </div>
-        <div className="total">
-          <p>Total</p>
-          <p className="totalprice">${total}</p>
-        </div>
-        <div className="footer">
-          <button onClick={clearCart}>Clear Cart</button>
         </div>
       </div>
     </div>
---
+++ src/components/TaskCard.js
@@ -0,0 +1,25 @@
+
+export const TaskCard = ({task, tasks, setTasks}) => {
+  function handleComplete(id){
+    const newTask = tasks.map((task)=>{
+      if(task.id === id){
+        task.complete = task.complete===true?false:true;
+      }
+      return task;
+    })
+    setTasks(newTask);
+  }
+  function handleRemove(id){
+    const newTask = tasks.filter((task)=>task.id!==id);
+    setTasks(newTask);
+  }
+  return (
+    <div className="flex justify-between items-center">
+      <div className="flex items-center gap-[10px]">
+        <input type="checkbox" onChange={()=>handleComplete(task.id)}/>
+        <p className={task.complete?"line-through":""}>{task.taskName}</p>
+      </div>
+        <button className="text-blue-600" onClick={()=>handleRemove(task.id)}>Remove</button>
+    </div>
+  )
+}
