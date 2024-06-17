68707ceab12f30ec90687d2b923716b3b1128869
Deepak Saraswat <deepaksaraswat238@gmail.com>
2024-06-17 08:27:48 +0530

    infinite scroll added

--- src/Hooks/useFetch.js
+++ src/Hooks/useFetch.js
@@ -2,15 +2,15 @@ import { useEffect, useState } from "react";
 
 export default function useFetch(apiPath, query, page){
     const [imageList, setList] = useState([]);
-    const url = `${apiPath}?${query?`query=${query}&`:""}${page?`page=${page}&`:""}per_page=30&client_id=${process.env.REACT_APP_API_KEY}`;
+    const url = `${apiPath}?${query?`query=${query}&`:""}${page?`page=${page}&`:""}per_page=15&client_id=${process.env.REACT_APP_API_KEY}`;
     useEffect(() => {
         async function fetchData() {
             const res = await fetch(url)
             const data = await res.json();
             console.log(data)
-            setList(data);
+            setList((prev)=> [...prev, ...data]);
         }
         fetchData();
-    }, [apiPath, query, url]);
+    }, [apiPath, query, url, page]);
     return {imageList}
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,21 +1,41 @@
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
 import { ImageCard } from "./ImageCard"
 import useFetch from "../Hooks/useFetch"
+import { useEffect } from "react"
+import { useState } from "react"
 
 export const Hero = () => {
-    const {imageList} = useFetch('https://api.unsplash.com/photos/', "", 2);
+    const [page, setPage] = useState(1);
+    console.log(document.documentElement.scrollTop);
+    const handleScroll = async () => {
+        try {
+            const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+            if (scrollTop + clientHeight >= scrollHeight - 20) {
+                if(page<5){
+                    setPage((prev)=>prev+1);
+                    console.log(page);
+                }
+            }
+        }catch (error) {
+        console.log(error);
+    }
+}
+useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+}, [])
+const { imageList } = useFetch('https://api.unsplash.com/photos/', "", page);
 
-    return (
-        <div className="mt-[2%]">
-            <div className="gallery px-[2%]">
-                <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 4 }}>
-                    <Masonry columnsCount={4} gutter="10px">
-                        {imageList && imageList.map((item) => (
-                            <ImageCard image={item.urls.regular} key={item.id} />
-                        ))}
-                    </Masonry>
-                </ResponsiveMasonry>
-            </div>
+return (
+    <div className="mt-[2%]">
+        <div className="gallery px-[2%]">
+            <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 4 }}>
+                <Masonry columnsCount={4} gutter="10px">
+                    {imageList && imageList.map((item, i) => (
+                        <ImageCard image={item.urls.regular} key={i} />
+                    ))}
+                </Masonry>
+            </ResponsiveMasonry>
         </div>
-    )
+    </div>
+)
 }
